b8b8bc1026012439f30f1f8758eddcf88582697f
Matthew Woodruff <mattpwoodruff@hotmail.com>
2017-02-01 07:53:25 -0500

    Update index.js
    Typo

--- examples/simple/index.js
+++ examples/simple/index.js
@@ -3,14 +3,14 @@ const path = require('path');
 
 const express = require('express');
 const webpack = require('webpack');
-const webapckUniversalMiddleware = require('webpack-universal-middleware');
+const webpackUniversalMiddleware = require('webpack-universal-middleware');
 
 const configs = require('./webpack.config');
 const multiCompiler = webpack(configs);
 
 const app = express();
 
-app.use(webapckUniversalMiddleware(multiCompiler));
+app.use(webpackUniversalMiddleware(multiCompiler));
 
 app.listen(3000);
 
